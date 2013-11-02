d8aae6232e7d2c5b9fe3ebcbf7c23b77e23e8226
Renlar <darklightning7@live.com>
2013-11-02 01:05:24 -0600

    updated gitignore

--- src/com/liddev/teleportmadness/Commands/Home.java
+++ src/com/liddev/teleportmadness/Commands/Home.java
@@ -1,7 +1,9 @@
 package com.liddev.teleportmadness.Commands;
 
 import com.liddev.teleportmadness.Commands.MadCommand;
+import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  *
@@ -11,7 +13,12 @@ public class Home implements MadCommand {
 
     @Override
     public boolean run(CommandSender sender, String[] args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Player p = (Player)sender;
+        Location l = p.getLocation();
+        if(args.length == 0){
+            
+        }
+        return false;
     }
     
 }
